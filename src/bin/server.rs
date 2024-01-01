a9b11dc42fb8bd7187aa74dafcd72fc555df54a2
Samuel Rayment <samrayment@gmail.com>
2024-01-01 21:58:44 +0000

    Adds command line args for proxying and ports

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,12 +1,25 @@
+use clap::{Parser};
+use pulcinella::server::{bind_socket, run_controlplane, run_mock, Mode, SequentialState};
 use std::net::SocketAddr;
-
 use tokio::join;
 use tracing::{info, level_filters::LevelFilter, Level};
 use tracing_subscriber::{EnvFilter, FmtSubscriber};
-use pulcinella::server::{bind_socket, run_controlplane, run_mock, Mode, SequentialState};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Opts {
+    #[clap(short, long, default_value = "false", env = "PROXY")]
+    proxy_mode: bool,
+    #[clap(short, long, default_value = "0", env = "CONTROL_PORT")]
+    control_port: u16,
+    #[clap(short, long, default_value = "0", env = "MOCK_PORT")]
+    mock_port: u16,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let opts = Opts::parse();
+
     let subscriber = FmtSubscriber::builder()
         .compact()
         .with_file(true)
@@ -20,20 +33,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .finish();
     tracing::subscriber::set_global_default(subscriber)?;
 
-    //let addr = SocketAddr::from(([127, 0, 0, 1], 0));
-    let control_plane_addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    let mock_addr = SocketAddr::from(([127, 0, 0, 1], 3001));
+    let control_addr = SocketAddr::from(([127, 0, 0, 1], opts.control_port));
+    let mock_addr = SocketAddr::from(([127, 0, 0, 1], opts.mock_port));
 
     let mock = bind_socket(mock_addr).await?;
-    let control_plane = bind_socket(control_plane_addr).await?;
+    let control = bind_socket(control_addr).await?;
     let state = SequentialState::new(mock.port);
 
-    info!("Control Plane on http://127.0.0.1:{}/", control_plane.port);
-    info!("Mock on http://127.0.0.1:{}/", mock.port);
+    info!("Control Port on http://127.0.0.1:{}/", control.port);
+    info!("{} on http://127.0.0.1:{}/", if opts.proxy_mode { "Proxy" } else { "Mock" }, mock.port);
 
-    let control_plane = run_controlplane(control_plane.listener, state.clone());
-    let proxy = run_mock(mock.listener, state, Mode::Proxy);
-    let (cp_result, mock_result) = join!(control_plane, proxy);
+    let control = run_controlplane(control.listener, state.clone());
+    let mock = run_mock(mock.listener, state, if opts.proxy_mode { Mode::Proxy } else { Mode::Mock });
+    let (cp_result, mock_result) = join!(control, mock);
 
     cp_result.and(mock_result)
 }
