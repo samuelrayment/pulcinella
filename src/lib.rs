8febc7dd4b9c63cad2fc7175017aa144aa15b9ca
Samuel Rayment <samrayment@gmail.com>
2024-01-01 14:51:09 +0000

    Collapses wasm and native client into client module

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,6 @@ pub mod interchange;
 #[cfg(feature = "server")]
 pub mod server;
 #[cfg(feature = "client")]
-pub(crate) mod shared_client;
-
-#[cfg(all(feature = "client", not(target_arch = "wasm32")))]
 pub mod client;
-#[cfg(all(feature = "client", any(target_arch = "wasm32", rust_analyzer)))]
-pub mod wasm_client;
-#[cfg(all(feature = "client", any(target_arch = "wasm32", rust_analyzer)))]
-pub use wasm_client as client;
+#[cfg(feature = "client")]
 pub(crate) mod network_client;
--- src/wasm_client.rs
+++
@@ -1,1 +0,0 @@
-pub use crate::shared_client::*;
